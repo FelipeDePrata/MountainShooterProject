c1bf1f68b3016b33758252d97365e4dd4c88bac4
Felipe José <felipe.jose.rj@gmail.com>
2025-08-12 17:20:16 -0300

    Adding package, classes and assets directory.

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pygame
+from code.Game import Game
 
-print('start')
-pygame.init()
-screen = pygame.display.set_mode(size=(600, 480))
-print('end')
-
-print('loop')
-while True:
-    #Check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #Close screen
-            quit()
+game = Game()
+game.run()
